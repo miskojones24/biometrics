27ca1230fb649974b77ea97e72da027ef2de1dad
miskojones24 <davidortizempleo@gmail.com>
2024-10-04 20:14:23 -0500

    Add files via upload
    Pulling changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function login() {
     const email = document.querySelector('.text-field[type="email"]').value;
     const password = document.querySelector('.text-field[type="password"]').value;
-
-
+    if (email === 'maicol' && password === '1234') {
+        window.location.href = 'index.html';
+    } else 
     if (email === 'admin' && password === '1234') {
         window.location.href = 'index.html';
     } else {
